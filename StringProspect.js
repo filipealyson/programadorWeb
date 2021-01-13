652cc4356a3796679a2e6966f2089bb6f29b7a15
filipealyson <77303771+filipealyson@users.noreply.github.com>
2021-01-13 19:03:27 -0300

    Add files via upload

---
+++ StringProspect.js
@@ -0,0 +1,6 @@
+//Propriedades das strings no JavaScript
+//Comprimento
+console.log('Este é o comprimento de uma string no JavaScript!'.length);//Comprimento
+console.log('ISTO É UMA STRING MAIÚSCULA PARA MINÚSCULA!'.toLowerCase());//Maiúscula para Minúscula
+console.log('isto é uma string minúscula para maiúscula!'.toUpperCase());//Minúscula para Maiúscula
+console.log('Organiza os espaços no íni    cio e fim do texto   !'.trim());//Minúscula para Maiúscula
---
+++ typeString.js
@@ -0,0 +1,26 @@
+//Tipe de dadis utlizados pelo JS: string
+console.log('Isso é uma string');
+console.log("Isso é uma string");
+console.log(`Usado também dentro de crase!`);
+
+console.log("It's seven o'clock");//aceita
+//console.log('Its's seven o'clock');// não aceita
+
+//console.log('Essa não pode ser como 'string');
+console.log('Essa pode ser como "string');
+//console.log('Essa não pode ser como 'string')
+console.log("Essa pode ser como 'string'");
+
+//Como imprimir caracteres especiais?
+console.log('Isso é um \'string!\'');
+console.log("Isso é um \"string\"");
+
+console.log(' \' '); //Imprimir aspa simples
+console.log(' \" '); //Imprimir aspa dupla
+console.log(' \\ '); //Imprimir aspa barra
+
+console.log('Esta é uma \nquebra de linha no JavaScript'); //Quebra de linha
+console.log('Esta é uma \rretorno de carro no JavaScript'); //Retorno de carro
+console.log('Esta é uma \vtabulação vertical no JavaScript'); //Tabulação vertical
+console.log('Esta é uma \vtabulação normal no JavaScript'); //Tabulação normal
+console.log('Esta é uma   \backspace no JavaScript'); //Backspace
